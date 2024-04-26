77050bd3f3642e21137df777682fe8f2a1153cef
pawan <thakunipawan@gmail.com>
2024-04-26 17:51:26 +0530

    commit

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,18 +1,30 @@
 import React, { useState } from 'react'
 
 const SignUp = () => {
-    const [name,setName]=useState(true)
+    const[fruits,setFruits]=useState(["apple","ball","cat"]);
+    const[filterFruits,setFilterFruits]=useState([])
 
-    const togglePassword=()=>{
-        setName(!name)
+    const filterfruitHandler=()=>{
+        setFilterFruits(fruits)
     }
-   return(
-    <div>
-    
-      <input type={name ?"text":"password" } value={name} onChange={(e)=>setName(e.target.value)}></input>
-      <button onClick={togglePassword}>togglepassword</button>
-    </div>
-   )
+
+
+    return (
+        <div>
+         <input type="text" value={filterFruits} onChange={(e)=>setFilterFruits(e.target.value)}></input>
+         <button onClick={filterfruitHandler}></button>
+         <p>
+           {
+            filterFruits.filter((e,i)=>{
+                return (
+                    <h key={i}>{e}</h>
+                )
+            })
+           }
+         </p>
+        
+        </div>
+    )
 
 }
 
