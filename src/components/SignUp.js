24a0918bc83c8d2133077aecdd39e26270af490d
pawan <thakunipawan@gmail.com>
2024-04-27 14:50:32 +0530

    commit

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,26 +2,30 @@ import React, { useState } from 'react'
 
 const SignUp = () => {
     const[fruits,setFruits]=useState(["apple","ball","cat"]);
-    const[filterFruits,setFilterFruits]=useState([])
+    const[filterFruits,setFilterFruits]=useState('')
 
-    const filterfruitHandler=()=>{
-        setFilterFruits(fruits)
-    }
+    
+             const filterfruit=fruits.filter((item)=>
+        item.toLowerCase().includes(filterFruits.toLowerCase())
+       )
 
+      
+    
 
     return (
         <div>
-         <input type="text" value={filterFruits} onChange={(e)=>setFilterFruits(e.target.value)}></input>
-         <button onClick={filterfruitHandler}></button>
-         <p>
-           {
-            filterFruits.filter((e,i)=>{
-                return (
-                    <h key={i}>{e}</h>
-                )
-            })
-           }
-         </p>
+         <input type="text"  onChange={(e)=>setFilterFruits(e.target.value)}></input>
+       <button onClick={()=>{
+        filterfruit.map((e,i)=>
+        (               
+                           <ul key={i}>{e}</ul>
+                       )
+                   )  
+                   setFilterFruits(filterFruits) 
+       }}></button>
+
+    
+         
         
         </div>
     )
